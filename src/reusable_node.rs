b3e8b27349a3699e3c17edbf9421a33d80b45bf9
Edoardo Morandi <morandidodo@gmail.com>
2020-04-10 17:07:13 +0200

    Remove clippy warns from reusable_node.rs

--- src/reusable_node.rs
+++ src/reusable_node.rs
@@ -24,33 +24,30 @@ pub struct StackEntry {
 }
 
 #[inline]
-pub unsafe extern "C" fn reusable_node_new() -> ReusableNode {
-    return {
-        let mut init = ReusableNode {
-            stack: {
-                let mut init = StackEntryArray {
-                    contents: std::ptr::null_mut::<StackEntry>(),
-                    size: 0 as os::raw::c_int as u32,
-                    capacity: 0 as os::raw::c_int as u32,
-                };
-                init
-            },
-            last_external_token: Subtree {
-                ptr: std::ptr::null::<SubtreeHeapData>(),
-            },
-        };
-        init
-    };
+pub(crate) unsafe extern "C" fn reusable_node_new() -> ReusableNode {
+    ReusableNode {
+        stack: StackEntryArray {
+            contents: std::ptr::null_mut::<StackEntry>(),
+            size: 0 as os::raw::c_int as u32,
+            capacity: 0 as os::raw::c_int as u32,
+        },
+        last_external_token: Subtree {
+            ptr: std::ptr::null::<SubtreeHeapData>(),
+        },
+    }
 }
 #[inline]
-pub unsafe extern "C" fn reusable_node_clear(mut self_0: *mut ReusableNode) {
+pub(crate) unsafe extern "C" fn reusable_node_clear(mut self_0: *mut ReusableNode) {
     (*self_0).stack.size = 0 as os::raw::c_int as u32;
     (*self_0).last_external_token = Subtree {
         ptr: std::ptr::null::<SubtreeHeapData>(),
     };
 }
 #[inline]
-pub unsafe extern "C" fn reusable_node_reset(mut self_0: *mut ReusableNode, mut tree: Subtree) {
+pub(crate) unsafe extern "C" fn reusable_node_reset(
+    mut self_0: *mut ReusableNode,
+    mut tree: Subtree,
+) {
     reusable_node_clear(self_0);
     array__grow(
         &mut (*self_0).stack as *mut StackEntryArray as *mut VoidArray,
@@ -59,18 +56,15 @@ pub unsafe extern "C" fn reusable_node_reset(mut self_0: *mut ReusableNode, mut 
     );
     let fresh5 = (*self_0).stack.size;
     (*self_0).stack.size = (*self_0).stack.size.wrapping_add(1);
-    *(*self_0).stack.contents.offset(fresh5 as isize) = {
-        let mut init = StackEntry {
-            tree: tree,
-            child_index: 0 as os::raw::c_int as u32,
-            byte_offset: 0 as os::raw::c_int as u32,
-        };
-        init
+    *(*self_0).stack.contents.offset(fresh5 as isize) = StackEntry {
+        tree,
+        child_index: 0 as os::raw::c_int as u32,
+        byte_offset: 0 as os::raw::c_int as u32,
     };
 }
 #[inline]
-pub unsafe extern "C" fn reusable_node_tree(mut self_0: *mut ReusableNode) -> Subtree {
-    return if (*self_0).stack.size > 0 as os::raw::c_int as os::raw::c_uint {
+pub(crate) unsafe extern "C" fn reusable_node_tree(mut self_0: *mut ReusableNode) -> Subtree {
+    if (*self_0).stack.size > 0 as os::raw::c_int as os::raw::c_uint {
         (*(*self_0)
             .stack
             .contents
@@ -80,26 +74,26 @@ pub unsafe extern "C" fn reusable_node_tree(mut self_0: *mut ReusableNode) -> Su
         Subtree {
             ptr: std::ptr::null::<SubtreeHeapData>(),
         }
-    };
+    }
 }
 #[inline]
-pub unsafe extern "C" fn reusable_node_byte_offset(mut self_0: *mut ReusableNode) -> u32 {
-    return if (*self_0).stack.size > 0 as os::raw::c_int as os::raw::c_uint {
+pub(crate) unsafe extern "C" fn reusable_node_byte_offset(mut self_0: *mut ReusableNode) -> u32 {
+    if (*self_0).stack.size > 0 as os::raw::c_int as os::raw::c_uint {
         (*(*self_0)
             .stack
             .contents
             .offset((*self_0).stack.size.wrapping_sub(1) as isize))
         .byte_offset
     } else {
-        4294967295 as os::raw::c_uint
-    };
+        4_294_967_295 as os::raw::c_uint
+    }
 }
 #[inline]
-pub unsafe extern "C" fn reusable_node_delete(mut self_0: *mut ReusableNode) {
+pub(crate) unsafe extern "C" fn reusable_node_delete(mut self_0: *mut ReusableNode) {
     array__delete(&mut (*self_0).stack as *mut StackEntryArray as *mut VoidArray);
 }
 #[inline]
-pub unsafe extern "C" fn reusable_node_advance(mut self_0: *mut ReusableNode) {
+pub(crate) unsafe extern "C" fn reusable_node_advance(mut self_0: *mut ReusableNode) {
     assert!(
         (*self_0)
             .stack
@@ -153,7 +147,7 @@ pub unsafe extern "C" fn reusable_node_advance(mut self_0: *mut ReusableNode) {
             .offset((*self_0).stack.size.wrapping_sub(1) as isize)
             as *mut StackEntry))
             .tree;
-        if !(ts_subtree_child_count(tree) <= next_index) {
+        if ts_subtree_child_count(tree) > next_index {
             break;
         }
     }
@@ -164,21 +158,18 @@ pub unsafe extern "C" fn reusable_node_advance(mut self_0: *mut ReusableNode) {
     );
     let fresh6 = (*self_0).stack.size;
     (*self_0).stack.size = (*self_0).stack.size.wrapping_add(1);
-    *(*self_0).stack.contents.offset(fresh6 as isize) = {
-        let mut init = StackEntry {
-            tree: *(*tree.ptr)
-                .c2rust_unnamed
-                .c2rust_unnamed
-                .children
-                .offset(next_index as isize),
-            child_index: next_index,
-            byte_offset: byte_offset,
-        };
-        init
+    *(*self_0).stack.contents.offset(fresh6 as isize) = StackEntry {
+        tree: *(*tree.ptr)
+            .c2rust_unnamed
+            .c2rust_unnamed
+            .children
+            .offset(next_index as isize),
+        child_index: next_index,
+        byte_offset,
     };
 }
 #[inline]
-pub unsafe extern "C" fn reusable_node_descend(mut self_0: *mut ReusableNode) -> bool {
+pub(crate) unsafe extern "C" fn reusable_node_descend(mut self_0: *mut ReusableNode) -> bool {
     assert!(
         (*self_0)
             .stack
@@ -199,25 +190,22 @@ pub unsafe extern "C" fn reusable_node_descend(mut self_0: *mut ReusableNode) ->
         );
         let fresh7 = (*self_0).stack.size;
         (*self_0).stack.size = (*self_0).stack.size.wrapping_add(1);
-        *(*self_0).stack.contents.offset(fresh7 as isize) = {
-            let mut init = StackEntry {
-                tree: *(*last_entry.tree.ptr)
-                    .c2rust_unnamed
-                    .c2rust_unnamed
-                    .children
-                    .offset(0 as os::raw::c_int as isize),
-                child_index: 0 as os::raw::c_int as u32,
-                byte_offset: last_entry.byte_offset,
-            };
-            init
+        *(*self_0).stack.contents.offset(fresh7 as isize) = StackEntry {
+            tree: *(*last_entry.tree.ptr)
+                .c2rust_unnamed
+                .c2rust_unnamed
+                .children
+                .offset(0 as os::raw::c_int as isize),
+            child_index: 0 as os::raw::c_int as u32,
+            byte_offset: last_entry.byte_offset,
         };
-        return 1 as os::raw::c_int != 0;
+        true
     } else {
-        return 0 as os::raw::c_int != 0;
-    };
+        false
+    }
 }
 #[inline]
-pub unsafe extern "C" fn reusable_node_advance_past_leaf(mut self_0: *mut ReusableNode) {
+pub(crate) unsafe extern "C" fn reusable_node_advance_past_leaf(mut self_0: *mut ReusableNode) {
     while reusable_node_descend(self_0) {}
     reusable_node_advance(self_0);
 }
